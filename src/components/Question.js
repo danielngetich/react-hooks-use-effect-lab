a3a9e79b8c3b071d448e5eccdd2cf5bf904eb9c8
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-10-04 20:11:22 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(()=>{
+    if(timeRemaining===0){
+      setTimeRemaining(0);
+      onAnswered(false)
+    }
+    const timer=setTimeout(()=>{
+      setTimeRemaining((timeRemaining)=>timeRemaining-1)
+    },1000);
+    return function (){
+      clearTimeout(timer)
+    }
+  })
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
